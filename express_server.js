9f83d8104fb28e3de54635b4bd348b45106de9b3
Nastasi Davydov <nastasidavydov@gmail.com>
2022-01-24 20:15:13 +0000

    Add an endpoint and register a handler on its path

--- express_server.js
+++ express_server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// register a handler on the root path
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
