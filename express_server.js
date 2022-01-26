f7f2d63068ab598f5c65b7647dd3ecfa8ed8ca4e
Nastasi Davydov <nastasidavydov@gmail.com>
2022-01-26 00:17:46 +0000

    Add a username var to templates and modify header partial to display username when logged in

--- express_server.js
+++ express_server.js
@@ -33,7 +33,10 @@ app.get("/", (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"],
+  };
   res.render('urls_index', templateVars);
 });
 
@@ -45,17 +48,25 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
   if (!urlDatabase[req.params.shortURL]) {
     res.sendStatus(418); // should be 404 :)
   } else {
-    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+    const templateVars = { 
+      shortURL: req.params.shortURL, 
+      longURL: urlDatabase[req.params.shortURL],
+      username: req.cookies["username"]
+    };
     res.render("urls_show", templateVars);
   }
 });
+
 
 app.post("/login", (req, res) => {
   const user = req.body.username;
